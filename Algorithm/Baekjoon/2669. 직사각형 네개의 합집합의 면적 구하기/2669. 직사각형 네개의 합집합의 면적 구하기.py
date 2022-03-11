17519f63d4e4fcb2994f1f78a9a9c4e65153577f
neweastK <eastshin2021@gmail.com>
2022-03-11 22:56:43 +0900

    0311

--- Algorithm/Baekjoon/2669. 직사각형 네개의 합집합의 면적 구하기/2669. 직사각형 네개의 합집합의 면적 구하기.py
+++ Algorithm/Baekjoon/2669. 직사각형 네개의 합집합의 면적 구하기/2669. 직사각형 네개의 합집합의 면적 구하기.py
@@ -1,8 +1,18 @@
 from pprint import pprint
-arr = [[0]*100 for _ in range(100)]
+arr = [[0]*10 for _ in range(10)]
 
 for _ in range(4) :
     loc = list(map(int,input().split()))
-    loc_1 = loc[:2]
-    loc_2 = loc[2:]
+    j1 = loc[0]
+    i1 = loc[1]
+    j2 = loc[2]
+    i2 = loc[3]
+    for i in range(j1,j2):
+        for j in range(i1,i2):
+            arr[i][j] = 1
 
+total = 0
+for a in arr :
+    total += sum(a)
+
+print(total)
--- Clone Coding/instagram/a.md
+++ Clone Coding/instagram/a.md
@@ -2,4 +2,9 @@ input 안에 글 보이게 하는건 placeholder 속성을 통해 한다
 
 
 
-floating labels 
+floating labels 쓰면 바로 로그인 창 만들 수 있더라 bootstrap을 적극 활용하자
+
+
+
+로그인 창을 input submit으로 했더니 제출 글이 떠서 곤란하다
+
