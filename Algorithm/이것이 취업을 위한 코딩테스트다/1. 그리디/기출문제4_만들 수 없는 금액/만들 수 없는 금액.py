d8a26ec57835ffe6ee6539a71678d6cd3ff34eef
neweastK <eastshin2021@gmail.com>
2022-04-28 23:47:42 +0900

    0428

--- Algorithm/이것이 취업을 위한 코딩테스트다/1. 그리디/기출문제4_만들 수 없는 금액/만들 수 없는 금액.py
+++ Algorithm/이것이 취업을 위한 코딩테스트다/1. 그리디/기출문제4_만들 수 없는 금액/만들 수 없는 금액.py
@@ -1,12 +1,13 @@
-str_nums = ['0001100','11111','00000001','11001100110011000001','11101101']
+N = 5
+coins = [3,2,1,1,9]
 
+coins.sort()
 
-for str_num in str_nums :
-    temp_one = str_num.split('0') #1으로 이루어진 문자열이 나오고 나머지는 ''로 나옴
-    count_one = (len(temp_one)-temp_one.count('')) # 1로 이루어진 집합의 개수
+set_coin = list(set(coins))
+coin_count = [0]*len(set(coins))
 
-    temp_zero = str_num.split('1') #0으로 이루어진 문자열이 나오고 나머지는 ''로 나옴
-    count_zero = (len(temp_zero)-temp_zero.count('')) # 0으로 이루어진 집합의 개수
+for i in range(1,len(set(coins)+1) :
 
-    ans = count_zero if count_zero<count_one else count_one
-    print(ans)
+    coin_count[i-1]
+
+print(coin_count)
