4e642d6b6106fa0d054775a1ecd7540fc500ca39
yslgitt <ysltub@gmail.com>
2022-05-23 02:33:05 +0900

    models

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -4,7 +4,15 @@ from django.conf import settings
 # Create your models here.
 
 
+class Boxoffice(models.Model):
+    title = models.CharField(max_length=100)
+    rank = models.IntegerField()
+    audiAcc = models.IntegerField()
+    independent = models.BooleanField()
+
+
 class Movie(models.Model):
+    tmdb_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=100)
     adult = models.BooleanField(default=False)
     independent = models.BooleanField()
@@ -24,6 +32,8 @@ class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
     content = models.TextField()
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_review')
+    rate = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -32,4 +42,22 @@ class Hashtag(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='hashtag')
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='hashtag')
     tag = models.CharField(max_length=100)
+
+
+class Actor(models.Model):
+    tmdb_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    birth = models.DateField()
+    profile_path = models.TextField()
+    movie = models.ManyToManyField(Movie, related_name='movie_actor')
+
+
+class Director(models.Model):
+    tmdb_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    birth = models.DateField()
+    profile_path = models.TextField()
+    movie = models.ManyToManyField(Movie, related_name='movie_director')  
+
+
 
