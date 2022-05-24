a5f46851f2efe32aef440b1c40c81f3b2ca7d7cf
neweastK <eastshin2021@gmail.com>
2022-05-24 14:50:52 +0900

    card_index

---
+++ final-pjt-front/src/components/MovieCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="card">
+      <img :src="imgUrl" class="card-img-top" alt="POSTER">
+      <div class="card-body">
+        <h5 class="card-title">{{ movie.title }}</h5>
+        <p class="card-text">{{ movie.overview }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: "MovieCard",
+  props: {
+    movie : Object
+  },
+  computed: {
+    imgUrl(){
+      const baseUrl = "https://image.tmdb.org/t/p/w500"
+      const videoId = this.movie.poster_path
+      return baseUrl + videoId
+    }
+  }
+} 
+</script>
+
+<style>
+  p {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+  }
+</style>
--- final-pjt-front/src/store/modules/movies.js
+++ final-pjt-front/src/store/modules/movies.js
@@ -1,19 +1,36 @@
+// import _ from 'lodash'
+import axios from 'axios'
+// import router from '@/router'
+// import drf from '@/api/drf'
+
+
 export default {
   state: {
+    // 영화 데이터들
+    movies : []
 
   },
 
   getters: {
+    movies : state => state.movies
 
   },
 
   mutations: {
-
+    SET_MOVIES: (state, movies) => state.movies = movies
   },
 
   actions: {
-
-  },
+    fetchMovies ({ commit,getters }) {    
+      axios({
+        url: 'http://127.0.0.1:8000/api/v1/movies/',
+        method: 'get',
+        headers: getters.authHeader,
+      })
+        .then(res => commit('SET_MOVIES', res.data))
+        .catch(err => console.error(err.response))
+  }
+  }
 
   
 
